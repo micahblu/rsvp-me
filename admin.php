8d4c3df5e689c7ec2c21c19eba9d48a20ad54386
Micah Blu <micahblu@gmail.com>
2013-09-14 15:23:29 -0700

    code clean up, indentation, getting rid of miscallaneous lines

--- admin.php
+++ admin.php
@@ -30,12 +30,9 @@ function rsvp_me_admin_header(){
 		};
 	
 		// since Wordpress 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-		$.post(ajaxurl, data, function(response) {
-									   
+		$.post(ajaxurl, data, function(response) { 
 			$("#eventrow_"+id).remove(); //remove deleted event row
-			
 			$("#rsvp_ajax_msg").html("Event successfully removed");
-			
 		});	
 	}
 		
@@ -77,49 +74,43 @@ function rsvp_me_events_overview(){
 	?>
 	<div id='admin-wrapper'>
 		<h1>RSVP ME Events Overview</h1>
-		
-		<?php
-		
-		global $wpdb;
-		
-		$sql = "SELECT * FROM " . $wpdb->prefix . "rsvp_me_events ORDER BY id DESC";
-		
-		$rows = $wpdb->get_results($sql);
-		
-		?>
-        <span id='rsvp_ajax_msg'></span>
-		<div id="admin-events-wrapper">	
-			<table cellpadding="0" cellspacing="0">
-				<tr>
-					<th>Event title</th>
-					<th>Venue</th>
-					<th>Date & time</th>
-                    <th>RSVPs</th>
-					<th></th>
-				</tr>
-				<?php
-				foreach($rows as $row){
-				
-					$rsvps = rsvp_me_get_respondents($row->id);					
-					$rsvp_count = count($rsvps);
-		
-					echo "<tr id='eventrow_" . $row->id . "'>\n";
-					echo "<td valign='top'>" . stripslashes($row->title) . "</td>\n";
-					echo "<td valign='top'>" . stripslashes($row->venue) . "</td>\n";
-					echo "<td valign='top'>" . date("F jS g:i a", strtotime($row->event_date_time)) . "</td>\n";
-					echo "<td valign='top'><a href='Javascript: toggle_rsvps($row->id)'>" . $rsvp_count . ($rsvp_count > 1 ? " people rsvpd" : " person has rsvpd") . "!</a></td>\n";
-					echo "<td><a href='?page=rsvp_me_edit_event&id=" . $row->id . "'>Edit</a> | ";
-					echo "<a href='Javascript: rsvp_me_delete_event(" . $row->id . ")' onclick='confirm(\"Are you sure you want to permanently delete this event?\")'>Delete</a></td>\n";
-					echo "</tr>\n";
+			<?php
+			global $wpdb;
+			$sql = "SELECT * FROM " . $wpdb->prefix . "rsvp_me_events ORDER BY id DESC";
+			$rows = $wpdb->get_results($sql);
+			?>
+      <span id='rsvp_ajax_msg'></span>
+			<div id="admin-events-wrapper">	
+				<table cellpadding="0" cellspacing="0">
+					<tr>
+						<th>Event title</th>
+						<th>Venue</th>
+						<th>Date & time</th>
+	          <th>RSVPs</th>
+						<th></th>
+					</tr>
+					<?php
+					foreach($rows as $row){
 					
-					rsvp_me_build_event_rsvps($rsvps, $row->id);
-					
-					echo "<tr><td colspan='5'><div style='width:100%; height:2px; border-bottom: 1px solid #ccc'></div></td></tr>\n";
-				}
-				?>
-			</table>
+						$rsvps = rsvp_me_get_respondents($row->id);					
+						$rsvp_count = count($rsvps);
+			
+						echo "<tr id='eventrow_" . $row->id . "'>\n";
+						echo "<td valign='top'>" . stripslashes($row->title) . "</td>\n";
+						echo "<td valign='top'>" . stripslashes($row->venue) . "</td>\n";
+						echo "<td valign='top'>" . date("F jS g:i a", strtotime($row->event_date_time)) . "</td>\n";
+						echo "<td valign='top'><a href='Javascript: toggle_rsvps($row->id)'>" . $rsvp_count . ($rsvp_count > 1 ? " people rsvpd" : " person has rsvpd") . "!</a></td>\n";
+						echo "<td><a href='?page=rsvp_me_edit_event&id=" . $row->id . "'>Edit</a> | ";
+						echo "<a href='Javascript: rsvp_me_delete_event(" . $row->id . ")' onclick='confirm(\"Are you sure you want to permanently delete this event?\")'>Delete</a></td>\n";
+						echo "</tr>\n";
+						
+						rsvp_me_build_event_rsvps($rsvps, $row->id);
+						
+						echo "<tr><td colspan='5'><div style='width:100%; height:2px; border-bottom: 1px solid #ccc'></div></td></tr>\n";
+					}
+					?>
+				</table>
 		</div>
-		
 	</div>
 	<? 
 }
@@ -180,9 +171,7 @@ function rsvp_me_add_event(){
 		
 		echo "<h2>Event added successfully</h2>\n";
 	}
-	
 	rsvp_me_event_form('add');
-
 }
 
 function rsvp_me_edit_event(){
@@ -198,14 +187,12 @@ function rsvp_me_edit_event(){
 		$date_time = $date . " " . ($meridian == "pm" ? ($hour += 12) : $hour) . ":" . $minute . ":00";			
 		
 		$wpdb->update( $wpdb->prefix . "rsvp_me_events", 
-					   array( 'title' => $title, 'description' => $description,
-							  'venue' => $venue, 'address' => $address, 
-							  'city' => $city, 'state' => $state,
-							  'zip' => $zip, 'event_date_time' => $date_time ), 
-					   array( 'id' => $id )
-					  );
-
-		
+	  	array( 'title' => $title, 'description' => $description,
+			  'venue' => $venue, 'address' => $address, 
+			  'city' => $city, 'state' => $state,
+			  'zip' => $zip, 'event_date_time' => $date_time ), 
+	   	array( 'id' => $id )
+	  );
 		echo "<h2>Event edited successfully</h2>\n";
 	}
 	
@@ -220,16 +207,10 @@ function rsvp_me_edit_event(){
 function rsvp_me_delete_event(){
 	
 	global $wpdb;
-	
 	$id = $wpdb->escape($_REQUEST['id']);
-	
 	$sql = "DELETE FROM " . $wpdb->prefix . "rsvp_me_events WHERE id='$id' LIMIT 1";
-	
 	$wpdb->query($sql);
 	
-	?>
-	<h2>Event successfully removed</h2>
-	<?
-
+	echo "<h2>Event successfully removed</h2>";
 }
 ?>
